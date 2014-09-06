24655b14693d21184d75dac42b35dd95a68dfa15
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-06 13:35:55 +0530

    Created the BNRReminderViewController.xib  file and added in necessary changes to BNRReminderViewController.m file in order to access the .xib file

--- HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/BNRReminderViewController.m
@@ -8,6 +8,17 @@
 
 #import "BNRReminderViewController.h"
 
+@interface BNRReminderViewController ()
+
+@property (nonatomic, weak) IBOutlet UIDatePicker *datePicker;
+
+@end
+
 @implementation BNRReminderViewController
 
+-(IBAction)addReminder:(id)sender
+{
+    NSDate *date = self.datePicker.date;
+    NSLog(@"Setting a reminder for %@", date);
+}
 @end
