20e74b9d2525f5c4ed01816772d313bb894c7efe
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-06 12:35:01 +0530

    Created BNRHypnosisViewController

---
+++ BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by Guwanjith Tennekoon on 9/6/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end
