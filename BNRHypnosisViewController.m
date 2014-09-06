891624aac2525fe204d066086c07e045dcf8ef3c
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-06 14:53:25 +0530

    Add in first tabBarImage and TabBar Title

--- BNRHypnosisViewController.m
+++ BNRHypnosisViewController.m
@@ -20,4 +20,21 @@
     //Set it as *the* view of this controller
     self.view = backgroundView;
 }
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        //Set the tab bar item's title
+        self.tabBarItem.title = @"Hypnotize";
+        
+        //Create a UIImage from a file
+        //This will use Hypno@2x.png on retina display devices
+        UIImage *image = [UIImage imageNamed:@"Hypno.png"];
+        
+        //Put that image on the tab bar item
+        self.tabBarItem.image = image;
+    }
+    return self;
+}
 @end
