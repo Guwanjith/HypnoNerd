49e132e8c1317afbdcadfd400d9633f2bb6656a7
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-06 13:29:07 +0530

    Created the BNRReminderViewController

---
+++ HypnoNerd/BNRReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.h
+//  HypnoNerd
+//
+//  Created by Guwanjith Tennekoon on 9/6/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRReminderViewController : UIViewController
+
+@end
---
+++ HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Guwanjith Tennekoon on 9/6/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end
