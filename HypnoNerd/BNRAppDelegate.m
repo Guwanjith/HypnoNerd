04ede2a0a6b29b0d7d99af95e26fad37b0e12d8b
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-06 15:02:07 +0530

    Removed unnecessary code from BNRAppDelegate

--- HypnoNerd/BNRAppDelegate.m
+++ HypnoNerd/BNRAppDelegate.m
@@ -20,11 +20,7 @@
     //Creating root view controller - if this line generates a warning, ignore it
     BNRHypnosisViewController *hvc = [[BNRHypnosisViewController alloc] init];
     
-    //This will get a pointer to an object that represents the app bundle
-    NSBundle *appBundle = [NSBundle mainBundle];
-    
-    //Look in the appBundle for the file BNRReminderViewController.xib
-    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] initWithNibName:@"BNRReminderViewController" bundle:appBundle];
+    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] init];
     
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
     tabBarController.viewControllers = @[hvc,rvc];
